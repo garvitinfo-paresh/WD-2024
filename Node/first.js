ad902b02c22226e8861ac36d7261e662712298e5
garvitinfo-paresh <garvitinfo.paresh@gmail.com>
2024-07-26 09:41:30 +0530

    26-07

---
+++ Node/first.js
@@ -0,0 +1,32 @@
+// let a = []
+// console.log(typeof(a))
+// console.log("1. Hello")
+// console.log("2. Hello")
+// console.log(10+20)
+// console.log('10'+20)
+// console.log('ten'+20)
+// console.log(10*20)
+// console.log(10/20)
+// console.log(120%3)
+
+// console.log('10'*20)
+// console.log('10'/20)
+// console.log('120'%3)
+
+// console.log(typeof(10))
+// console.log(typeof('10'))
+
+// console.log('10'+20)
+// console.log('ten'*20)  // NaN  -> Not a Number 
+// console.log(typeof NaN)
+
+// true =  1
+// false = 0
+
+// console.log(true+true)
+// console.log(+"true" * 1)
+console.log( 1 +  +"true" )
+// console.log(true+ +'true')
+// console.log(true*'true')
+
+// console.log(10+ +'20')
